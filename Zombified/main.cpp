d0ae262f53c8bf1f1d62a71f12328e62de479980
HAAKS <heba.salama18@gmail.com>
2015-11-02 03:22:53 +0200

    Initial Commit

---
+++ Zombified/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Zombified
+//
+//  Created by Heba A. Salama on 11/2/15.
+//  Copyright (c) 2015 Heba A. Salama. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
