7cafda366fb76b386a304def3720c9d1f2f54d2f
HAAKS <heba.salama18@gmail.com>
2015-11-17 23:52:49 +0200

    Added Multiple Attackers

--- Zombified/main.cpp
+++ Zombified/main.cpp
@@ -6,7 +6,7 @@
 
 void Gatherer();
 void Defender(); //draws the defender
-void Attacker();
+void Attacker(int t);
 void Grid();
 void Keyboard(unsigned char key, int x, int y);
 void HighlightTheTile();
@@ -14,7 +14,7 @@ void Timer(int value);
 void IsPresent(); //keeps track of where defenders & RG's are
 void Gettingcorrespondingaxes(int row, int column); //have the row and column and get the coordinates
 void Hit(); //checks for the collision and fires the bullets
-int Settingrowsdead(int coresponsingrow);//KILLS THE ROW
+int  Settingrowsdead(int coresponsingrow);//KILLS THE ROW
 void Killtherow(int n); //overlaps the whole row with a rectangle to kill it
 void Display();
 void Anim();
@@ -23,7 +23,9 @@ void DScreen(std::string string, int x, int y);
 void RGScreen(std::string string, int x, int y);
 void EndGame(std::string string);
 
-float zoomin=200;
+float lookfirst=0;
+float looksecond =20.0;
+float lookthird=25.0;
 float rotangle;
 bool defenders [6][10]; //to keep track which tile has defenders
 bool resourcegatherers [6][10];
@@ -33,20 +35,22 @@ int row;
 int column;
 bool destroyedrows [10]; //checks which row is destroyed
 GLboolean pause = false;
-float moveattackerx =6;
-int moveattackerz =0;
+GLboolean cameraview = false;
+float moveattackerx [200];
+int moveattackerz[200] ;
+int cattacker =0;
 int cordinates[2]; //carrying the coordinates from rows and columns
 int x;
 int z;
 int kills; //calculate how many attackers died
-int hits; //calculate hit times of the bullet lel attacker
+int hits [200]; //calculate hit times of the bullet lel attacker
 float movebullet;
 int checkforeven;//for random numbers
 int a ; //looper variable for getting axes of defenders
 int b ; //looper variable for getting axes of defenders
-float R =0;//color of car
-float G =0;
-float B =0;
+float R [200];//color of car
+float G [200];
+float B [200];
 int attackerwin=0; //a counter to keep track of how many times the attacker reached the house
 int resourceholder =100; //to get the final value Resources(R) available
 int addresourceforgatherers=0;
@@ -84,7 +88,6 @@ void Keyboard(unsigned char key, int x, int y){
         if(destroyedrows[temp] == false && resourceholder>=50){
             resourceholder =resourceholder-50;
             resourcegatherers[row][column] = true;
-         //   bullet[row][column]=true;
             addresourceforgatherers++;
         }
         column =0;
@@ -107,10 +110,14 @@ void Keyboard(unsigned char key, int x, int y){
     else if(valueofchar > 0 && valueofchar <10 && row !=0) {
         column=key-48;
             }
+    if(key=='v'){
+        cameraview=true;
+    }
     glutPostRedisplay();
 }
 //checkes when defender/RG and attacker collides.
 void IsPresent(){
+    int temp;
     for(a =1;a<6;a++){
         for(b=1;b<10;b++){
             if(defenders[a][b] == true){
@@ -120,8 +127,10 @@ void IsPresent(){
                  Defender();
                  glPopMatrix();
                  Hit();
-                if((cordinates[1]-1 ==moveattackerz) && ((cordinates[0]+0.5) >= moveattackerx)) {
+                for(temp=0;temp<cattacker;temp++){
+                if((cordinates[1]-1 ==moveattackerz[temp]) && ((cordinates[0]+0.5) >= moveattackerx[temp])) {
                     defenders[a][b]=false;
+                }
                 }
                         }
             if(resourcegatherers[a][b] == true){
@@ -130,17 +139,18 @@ void IsPresent(){
                 glTranslatef(cordinates[0]+0.5, 0, cordinates[1]-1.5);
                 Gatherer();
                 glPopMatrix();
-                if((cordinates[1]-1 ==moveattackerz) && ((cordinates[0]+0.5) >= moveattackerx)) {
+                for(temp=0;temp<cattacker;temp++){
+                if((cordinates[1]-1 ==moveattackerz[temp]) && ((cordinates[0]+0.5) >= moveattackerx[temp])) {
                     resourcegatherers[a][b]=false;
                         addresourceforgatherers--;
-                }
-                
+                }}
             }
         }
     }
 }
 
 void Hit(){
+    int temp;
     float holderofbullet = cordinates[0]+0.7+movebullet;
     if(movebullet==0){
         bullet[a][b]=true;
@@ -151,30 +161,32 @@ void Hit(){
     glTranslatef(holderofbullet, 1, cordinates[1]-1.5);
     glutSolidSphere(0.25, 7, 7);
     glPopMatrix();
-    if((moveattackerz+1) == cordinates[1]){
-    if(((holderofbullet<=0)) && ((moveattackerx)<=0) && (moveattackerx>-10)){
-        if((moveattackerx)+(abs(holderofbullet))<0){
-            hits++;
-            R+=0.2;
-            G+=0.2;
-            B+=0.2;
+    for(temp=0;temp<cattacker;temp++){
+    if((moveattackerz[temp]+1) == cordinates[1]){
+    if(((holderofbullet<=0)) && ((moveattackerx[temp])<=0) && (moveattackerx[temp]>-10)){
+        if((moveattackerx[temp])+(abs(holderofbullet))<0){
+            hits[temp]++;
+            R[temp]+=0.2;
+            G[temp]+=0.2;
+            B[temp]+=0.2;
             bullet[a][b]=false;
         }
     }
-    else if ((holderofbullet>0) && (moveattackerx>0)){
-        if((holderofbullet-moveattackerx)<0.000001){
-            hits++;
-            R+=0.2;
-            G+=0.2;
-            B+=0.2;
+    else if ((holderofbullet>0) && (moveattackerx[temp]>0)){
+        if((holderofbullet-moveattackerx[temp])<0.000001){
+            hits[temp]++;
+            R[temp]+=0.1;
+            G[temp]+=0.1;
+            B[temp]+=0.1;
             bullet[a][b]=false;
         }
 }
-        if(hits==4){
-            moveattackerz = 0;
+        if(hits[temp]==4){
+            moveattackerz[temp] = 0;
             kills++;
             resourceholder =resourceholder+20;
         }
+    }
 }
 }
 
@@ -185,7 +197,7 @@ void Grid(){
     glTranslatef(-10, 0, 0);
     for(first =0;first<=18;first+=2){
         glBegin(GL_LINE_LOOP);
-        glColor3f(0, 0, 0);
+        glColor3f(0.47, 0.569, 0.302);
         glVertex3f(first, 0, 0);
         glVertex3f(first, 0,10);
         glEnd();
@@ -200,38 +212,50 @@ void Grid(){
 }
 
 void Display(void) {
-    int counter;
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    int counter;
+    int temp;
     Grid();
     IsPresent();
     if(row !=0 && column !=0)
     {
         HighlightTheTile();
     }
-    if(moveattackerz!=0){ //after attackerwin++ it doesn't enter again until a new attacker is present
-    if(moveattackerx >=-11.4){ //moves as long as the object didnt reach the end
-    glPushMatrix();
-    glTranslatef(moveattackerx, 1, moveattackerz);
-    Attacker();
-    glPopMatrix();
-
+    for(temp=0;temp<cattacker;temp++){
+        if(moveattackerz[temp]!=0){ //after attackerwin++ it doesn't enter again until a new attacker is present
+            if(moveattackerx[temp] >=-11.4){ //moves as long as the object didnt reach the end
+                glPushMatrix();
+                glTranslatef(moveattackerx[temp], 0.5, moveattackerz[temp]);
+                Attacker(temp);
+                glPopMatrix();
+                
+            }
+            
+            else if(moveattackerx[temp]<-11.5){
+                destroyedrows[moveattackerz[temp]] =true;
+                attackerwin++;
+                moveattackerz[temp] =0;
+            }
+        }
     }
-        
-    else if(moveattackerx<-11.5){ //check here the BUG !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ANA HENA
-        destroyedrows[moveattackerz] =true;
-        attackerwin++;
-        moveattackerz =0;
-    }
-    }
+ 
     for(counter =1;counter<12;counter+=2){
         if(destroyedrows[counter]==true){
             Killtherow(counter);
         }
     }
     if(attackerwin ==3){
-       EndGame("Game is Over ya Amoor ! :D");
+       EndGame("Game is Over :D");
         pause = true;
     }
+    if(kills==50){
+        EndGame("You won! El Sala 3al Zeen :D");
+        pause = true;
+    }
+    
+    gluLookAt(lookfirst, looksecond, lookthird, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
     Counter(-10, 10);
     DScreen("25", -6, 10); //display value of Defender
     RGScreen("50", 0, 10);//display value of RG
@@ -239,8 +263,45 @@ void Display(void) {
     glFlush();
 }
 
+void Anim() {
+    int temp;
+    if(!pause){
+        rotangle+=2;
+        for(temp=0;temp<cattacker;temp++){
+            if(moveattackerz[temp] !=0){
+                moveattackerx[temp]-=0.1;
+            }
+        }
+        if(movebullet >= 17){
+            movebullet=0;
+        }
+        else movebullet+=0.5;
+        
+    }
+    if(cameraview){
+        if(lookfirst<360){
+            lookfirst+=1.5;
+            lookfirst=cos(lookfirst*0.5)+lookfirst;
+            //looksecond=sin(looksecond*0.5);
+    }
+        else{
+            if(looksecond<80)
+                lookfirst+=0.5;
+            else{
+                
+            }
+            //            else{
+            //                if(lookthird<40)
+            //                lookthird+=0.5;
+            //            }
+        }
+    }
+    glutPostRedisplay();
+}
+
 void Killtherow(int counter){
     glPushMatrix();
+    glColor3f(0.47, 0.569, 0.302);
     glBegin(GL_POLYGON);
     glVertex3f(8, 0, counter-1);
     glVertex3f(8, 0, counter+1);
@@ -249,22 +310,6 @@ void Killtherow(int counter){
     glEnd();
     glPopMatrix();
 }
-
-//int Settingrowsdead(int h){
-//    switch(h){
-//        case 1: n=1;
-//            break;
-//        case 3: n=2;
-//            break;
-//        case 5: n=3;
-//            break;
-//        case 7: n=4;
-//            break;
-//        case 9: n=5;
-//            break;
-//    }
-//    return n;
-//}
 
 void HighlightTheTile(){
         Gettingcorrespondingaxes(row, column);
@@ -292,40 +337,29 @@ void Gettingcorrespondingaxes(int row, int column){
      cordinates[1] = z;
 }
 
-void Anim() {
-    if(!pause){
-    rotangle+=2;
-    if(moveattackerz !=0){
-        moveattackerx-=0.1;
-        std::cout<<" HERE IAM HENAA5: " << moveattackerx;
-    }
-        if(movebullet >= 17){
-            movebullet=0;
-        }
-        else movebullet+=0.5;
-    }
-    glutPostRedisplay();
-}
-
 void Timer(int value) {
-    hits=0;
-    R=0;
-    G=0;
-    B=0;
+    hits[cattacker]=0;
+    R[cattacker]=0;
+    G[cattacker]=0;
+    B[cattacker]=0;
     do{
      checkforeven = rand() % 10; //gets the random number to check later if even or odd
-     moveattackerz = checkforeven ; //get a random z number
-    if(moveattackerz ==0) {//random number is 0, then make the z =1
-        moveattackerz =1;
+     moveattackerz[cattacker] = checkforeven ; //get a random z number
+    if(moveattackerz[cattacker] ==0) {//random number is 0, then make the z =1
+        moveattackerz[cattacker] =1;
         checkforeven =1;
     }
     else if(checkforeven%2==0){
-        moveattackerz-=1;
+        moveattackerz[cattacker]-=1;
     }
     }
-    while(destroyedrows[moveattackerz]==true);
-    moveattackerx=6; // re-initialize the x coordinate when the function is called so the attacker appear
-    if(timeforresource==1){
+        while(destroyedrows[moveattackerz[cattacker]]==true); //check destroyedrows
+    moveattackerx[cattacker]=6; // re-initialize the x coordinate when the function is called so the attacker appear
+    R[cattacker]= 0.914;
+    G[cattacker]= 0.678;
+    B[cattacker]= 0.192;
+    cattacker++;
+    if(timeforresource==5){
         resourceholder= resourceholder+ addresourceforgatherers*10;
         timeforresource=0;
     }
@@ -333,15 +367,14 @@ void Timer(int value) {
         timeforresource ++; //checks for time to see when to increment resourceholders for RG
     }
     glutPostRedisplay();
-    glutTimerFunc(10 * 1000, Timer, 0);
-    
+    glutTimerFunc(4 * 1000, Timer, 0);
 }
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitWindowSize(900, 1000);
     glutInitWindowPosition(150, 150);
-    glutCreateWindow("OpenGL - 3D Template");
+    glutCreateWindow("");
     glutDisplayFunc(Display);
     glutKeyboardFunc(Keyboard);
     glutTimerFunc(0, Timer, 0);
@@ -352,9 +385,6 @@ int main(int argc, char** argv) {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluPerspective(45.0f, 300 / 300, 0.1f, 300.0f);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    gluLookAt(0.0f, 20.0f, 25.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
     glutMainLoop();
 }
 
@@ -373,7 +403,6 @@ void Gatherer(){
         glTranslatef(0, 0.01*i, 0);
         glRotatef(i, 0, 1, 0);
         glTranslatef(2, 0, 0);
-        glColor3f(0.5f, 0.5f, 0.5f);
         glutSolidSphere(0.90, 2, 2);
         glPopMatrix();
     }
@@ -382,6 +411,7 @@ void Gatherer(){
 
 void Defender(){
     glPushMatrix();
+    glColor3f(0.49, 0.729, 0.74);
     glRotatef(90, 0, 1, 0);
     glScaled(0.22, 0.22, 0.22);
     glPushMatrix();
@@ -409,13 +439,13 @@ void Defender(){
     glPopMatrix();
 }
 
-void Attacker(){
+void Attacker(int y){
     GLUquadric* qobj;
     qobj = gluNewQuadric();
     glPushMatrix(); //Matrix of the Big Cube, Body of the Car
     glScaled(0.7, 0.5, 0.5);
     glTranslatef(2.5, 0, 0);
-    glColor3f(R, G, B);
+    glColor3f(R[y], G[y], B[y]);
     glutSolidCube(1.5);
     glPopMatrix();
     glPushMatrix(); //The next four matrices are for the wheels
@@ -445,21 +475,21 @@ void Attacker(){
     glPopMatrix();
     glPushMatrix();
     glScaled(0.15, 0.15, 0.15);
-    glColor3f(R, G, B);
+    glColor3f(R[y], G[y], B[y]);
     glTranslatef(8.5, 0, -1.5);
     glRotatef(270, 0, 1, 0);
     gluCylinder(qobj, 0.8, 0, 2, 70, 70);
     glPopMatrix();
     glPushMatrix();
     glScaled(0.15, 0.15, 0.15);
-    glColor3f(R, G, B);
+    glColor3f(R[y], G[y], B[y]);
     glTranslatef(8.5, 0, 0);
     glRotatef(270, 0, 1, 0);
     gluCylinder(qobj, 0.8, 0, 2, 70, 70);
     glPopMatrix();
     glPushMatrix();
     glScaled(0.15, 0.15, 0.15);
-    glColor3f(R, G, B);
+    glColor3f(R[y], G[y], B[y]);
     glTranslatef(8.5, 0, 1.5);
     glRotatef(270, 0, 1, 0);
     gluCylinder(qobj, 0.8, 0, 2, 70, 70);
